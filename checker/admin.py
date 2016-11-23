9d45d0ea13574f2c3199dc22397b5154535c1630
phil9l <phil9lne@gmail.com>
2016-11-23 14:29:16 +0500

    Different flags for teams

--- checker/admin.py
+++ checker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Task, Hint
+from .models import Task, Hint, Flag
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(Hint)
+admin.site.register(Flag)
--- checker/models.py
+++ checker/models.py
@@ -10,7 +10,6 @@ class Task(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
     price = models.PositiveIntegerField()
-    flag = models.CharField(max_length=64)
     teams = models.ManyToManyField(Team, related_name='tasks', blank=True)
 
     correct_flag_message = models.TextField()
@@ -23,6 +22,12 @@ class Task(models.Model):
         team.tasks_number += 1
         team.balance += self.price
         team.save()
+
+    def _get_flag(self, team):
+        flag = Flag.objects.filter(team=team, task=self).first()
+        if flag is None:
+            return None
+        return flag.flag
 
     def is_solved(self, team):
         return self.teams.filter(pk=team.pk).exists()
@@ -40,19 +45,22 @@ class Task(models.Model):
                     'is_correct': False}
         if not team.contest_started():
             return {'message': 'Contest is not started.', 'is_correct': False}
+        correct_flag = self._get_flag(team)
+        if correct_flag is None:
+            return {'message': 'No flag for you. :(', 'is_correct': False}
 
-        correct = self.flag == flag
-        message = self.correct_flag_message if correct else \
+        is_correct = correct_flag == flag
+        message = self.correct_flag_message if is_correct else \
             self.wrong_flag_message
 
         submit = Submit(team=team, flag=flag)
         submit.save()
 
-        if not self.is_solved(team) and correct:
+        if not self.is_solved(team) and is_correct:
             self._award_team(team)
             self.teams.add(team)
 
-        return {'message': message, 'is_correct': correct}
+        return {'message': message, 'is_correct': is_correct}
 
 
 class Hint(models.Model):
@@ -85,3 +93,12 @@ class Submit(models.Model):
     team = models.ForeignKey(Team)
     flag = models.CharField(max_length=100)
     time = models.DateTimeField(auto_now_add=True)
+
+
+class Flag(models.Model):
+    team = models.ForeignKey(Team)
+    task = models.ForeignKey(Task)
+    flag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}'s {} flag".format(self.team.user.username, self.task.title)
