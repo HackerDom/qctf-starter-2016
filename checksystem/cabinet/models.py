6a311b5178e400ca0d1b55c06298de9e1b54ebef
phil9l <phil9lne@gmail.com>
2016-11-24 18:57:00 +0500

    Add regions

--- checksystem/cabinet/models.py
+++ checksystem/cabinet/models.py
@@ -8,19 +8,30 @@ from django.utils import timezone
 from django.conf import settings
 
 
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
 class Team(models.Model):
     name = models.CharField(max_length=100)
     balance = models.PositiveIntegerField(default=0)
     tasks_number = models.PositiveIntegerField(default=0)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     replace_info = models.TextField(blank=True, default='[]')
-    start_time = models.DateTimeField(default=timezone.now)
+    region = models.ForeignKey(Region)
 
     def __str__(self):
         return self.name
 
     def get_name(self):
         return self.name
+
+    def get_start_time(self):
+        return self.region.start_time
 
     def spend_money(self, number):
         self.balance -= number
@@ -31,10 +42,11 @@ class Team(models.Model):
         return teams + 1
 
     def contest_started(self):
-        return timezone.now() >= self.start_time
+        return timezone.now() >= self.get_start_time()
 
     def contest_finished(self):
-        return timezone.now() > self.start_time + settings.CONTEST_DURATION
+        finish_time = self.get_start_time() + settings.CONTEST_DURATION
+        return timezone.now() > finish_time
 
 
 @receiver(post_save, sender=User)
