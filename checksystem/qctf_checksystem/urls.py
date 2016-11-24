78557f0eff1a31d9a1dc20feac70b3cccc1368bc
phil9l <phil9lne@gmail.com>
2016-11-24 18:37:38 +0500

    Edit routing: /checker/ on /

--- checksystem/qctf_checksystem/urls.py
+++ checksystem/qctf_checksystem/urls.py
@@ -17,16 +17,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView, RedirectView
+# from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^profile/', include('cabinet.urls')),
-    url(r'^checker/', include('checker.urls')),
+    url(r'^', include('checker.urls')),
     # url(r'^$', TemplateView.as_view(
     #     template_name='qctf_checksystem/index.html')),
-    url(r'^$', RedirectView.as_view(url='/checker/', permanent=False),
-        name='index')
+    # url(r'^$', RedirectView.as_view(url='/checker/', permanent=False),
+    #     name='index')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
