b2d671a2fef9b21fa6fc975a2361780cc6041ad6
phil9l <phil9lne@gmail.com>
2016-11-25 19:48:29 +0500

    Removed transparency

--- checksystem/static/js/get-shit-done.js
+++ checksystem/static/js/get-shit-done.js
@@ -80,29 +80,3 @@ $(function () {
     
 });
 
-$(document).scroll(function() {
-    if( $(this).scrollTop() > 260 ) {
-        if(transparent) {
-            transparent = false;
-            $('nav[role="navigation"]').removeClass('navbar-transparent');
-        }
-    } else {
-        if( !transparent ) {
-            transparent = true;
-            $('nav[role="navigation"]').addClass('navbar-transparent');
-        }
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
