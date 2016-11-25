f1011694b67a632b7230b1d2fe0a244350ef2e57
phil9l <phil9lne@gmail.com>
2016-11-25 19:02:30 +0500

    Legend

--- checksystem/checker/urls.py
+++ checksystem/checker/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^scoreboard/$', views.scoreboard, name='scoreboard'),
-    url(r'^hints/$', views.hints, name='hints'),
+    url(r'^market/$', views.hints, name='hints'),
     url(r'^check_flag/(?P<task_id>\d+)/$', views.check_flag,
         name='check_flag'),
     url(r'^hints/(?P<hint_id>\d+)/buy/$', views.buy_hint,
