a3cbbc5ada85a0409197a0413acd3d31f1ee982d
Andrew Gein <andgein@yandex.ru>
2016-11-27 01:50:12 +0500

    Admin models

--- checksystem/checker/admin.py
+++ checksystem/checker/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
 from .models import Task, Hint, Flag, Submit
 
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(Hint)
-admin.site.register(Flag)
+
+class HintAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'name', 'description', 'price')
+    search_fields = ('task__title', 'name', 'description')
+
+admin.site.register(Hint, HintAdmin)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'price', 'default_flag')
+    search_fields = ('title', 'default_flag')
+
+admin.site.register(Task, TaskAdmin)
+
+
+class FlagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'team', 'task', 'flag')
+    list_filter = ('team', )
+    search_fields = ('team__name', 'task__title', 'flag')
+
+admin.site.register(Flag, FlagAdmin)
 
 
 class SubmitAdmin(admin.ModelAdmin):
