68425b709563c1391fdde70f12311f77f69febeb
mpivko <mpivko@yandex.ru>
2016-11-27 05:41:47 +0500

    Fix problem with loading users from dump

--- checksystem/cabinet/models.py
+++ checksystem/cabinet/models.py
@@ -64,10 +64,14 @@ class Team(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    if 'raw' in kwargs and kwargs['raw']:
+        return
     if created:
         Team.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    if 'raw' in kwargs and kwargs['raw']:
+        return
     instance.team.save()
