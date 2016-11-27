ab1c3257bb6c75c7e74ef0d5d6c3e7ca20dd4f26
Dmitry Zubarev <mathcrosp@yandex.ru>
2016-11-27 05:56:44 +0500

    Added DNA; updated configs.

--- deploy/simple-web/deploy.sh
+++ deploy/simple-web/deploy.sh
@@ -3,12 +3,13 @@
 SRC="/home/simple-web/qctf-starter-2016"
 DST="/var/www"
 
-mkdir -p $DST/{badip,clicker,grouping-s,guide}/www
+mkdir -p $DST/{badip,clicker,grouping-s,guide,dna}/www
 
 cp -R $SRC/badip/* $DST/badip/www
 cp -R $SRC/destructive-clicker/* $DST/clicker/www
 cp -R $SRC/kurlyandian/* $DST/grouping-s/www
 cp -R $SRC/installation-guide/* $DST/guide/www
+cp -R $SRC/dna-encoding/dna_checker/* $DST/dna/www
 
 mkdir -p $DST/deploy
 
