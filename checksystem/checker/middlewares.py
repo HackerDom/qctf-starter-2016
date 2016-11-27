ad148f2e1e274938d61b92d3c04b00e2dba874db
mpivko <mpivko@yandex.ru>
2016-11-27 05:54:48 +0500

    Fix showing legend at first login

--- checksystem/checker/middlewares.py
+++ checksystem/checker/middlewares.py
@@ -1,7 +1,8 @@
 class FirstVisitMiddleware(object):
     def process_request(self, request):
         request.first_visit = False
-        if request.user.is_authenticated and request.method == 'GET' and not request.COOKIES.get('visited', False):
+        if request.user.is_authenticated and request.method == 'GET' and not request.COOKIES.get('visited', False) and \
+            request.user.team.contest_started():
             request.first_visit = True
 
     def process_response(self, request, response):
