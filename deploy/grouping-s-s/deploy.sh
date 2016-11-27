f338835d92a7d606d81fa7c479d0188a9f56b2ea
Dmitry Zubarev <mathcrosp@yandex.ru>
2016-11-27 05:08:00 +0300

    Updated deployment code.

--- deploy/grouping-s-s/deploy.sh
+++ deploy/grouping-s-s/deploy.sh
@@ -5,8 +5,10 @@ DST="/var/www"
 
 mkdir -p $DST/grouping-s/www
 
-cp -R $SRC/kurlyandian/* $DST/grouping-s/www
+cp -R $SRC/kurlyandian/destroy $DST/grouping-s/www
+cp -R $SRC/kurlyandian/explore $DST/grouping-s/www
+cp -R $SRC/kurlyandian/learn $DST/grouping-s/www
 
-SRC="$SRC/deploy/simple-web"
+SRC="$SRC/deploy/grouping-s-s"
 
 cp $SRC/nginx.conf /etc/nginx/nginx.conf
