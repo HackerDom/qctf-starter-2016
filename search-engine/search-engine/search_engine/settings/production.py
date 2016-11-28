a3597d05b36a4db3f9ba7dbcf2bb519c61172d69
Alexander Borzunov <hxrussia@gmail.com>
2016-11-28 21:55:52 +0500

    Deploy Elasticsearch on the same machine

--- search-engine/search-engine/search_engine/settings/production.py
+++ search-engine/search-engine/search_engine/settings/production.py
@@ -6,7 +6,7 @@ AUTH_SERVICE_URI = 'http://auth.local'
 MONGO_URI = 'mongodb://mongo:27017/'
 MONGO_DB_NAME = 'search_engine'
 
-ES_HOSTS = ['77.244.215.165:9200']
+ES_HOSTS = ['elasticsearch:9200']
 ES_INDEX_NAME = 'search_engine'
 
 CRAWLER_THREADS = 40
